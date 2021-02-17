534394052bdd5ae181b29ae58822ddcc786a4052
Kaushlendra Rai <rai.kaushal@gmail.com>
2021-02-17 20:32:05 +0530

    Partition array in linear time around pivot

--- src/com/kausha/algo/PartitionArrayLinearTime.java
+++ src/com/kausha/algo/PartitionArrayLinearTime.java
@@ -10,36 +10,34 @@ public class PartitionArrayLinearTime {
 	public static void main(String[] args) {
 		int[] arr = {77, 1, 3, 89, 4, 75, 48, 42,36, 89, 222};
 		int num = 6;
-		partitionArray(arr, num);
+		System.out.println("Final location of partition pivot: " + partitionArray(arr, num));
 		
 		for(int i=0; i < arr.length; i++)
 			System.out.print(arr[i] + " ");
-
 	}
 
 	public static int partitionArray(int[] arr, int index) {
 		int num = arr[index];
+		System.out.println("Pivot element: " + num);
 		int low = 0;
 		int high = arr.length-1;
-		int current = 0;
 		
 		while(low<high){
-			if(arr[current] < num){
+			if(arr[low] < num){
 				low++;
-				current++;
 			}else if(arr[high] > num){
 				high--;
-			}else if(arr[current] > num){
+			}else if(arr[low] > num){
 				// Swap current & num at high
-				int temp = arr[current];
-				arr[current] = arr[high];
+				int temp = arr[low];
+				arr[low] = arr[high];
 				arr[high] = temp;
 				
+				low++;
 				high--;
 			}
 		}
 		
 		return low;
 	}
-
 }
