cd9bac4953670f11bd048fa6c0bf44be53976af9
Kaushlendra Rai <rai.kaushal@gmail.com>
2021-11-10 10:14:11 +0530

    EditDistance and email-notification design

--- src/com/kausha/algo/dp/EditDistanceStrings.java
+++ src/com/kausha/algo/dp/EditDistanceStrings.java
@@ -5,36 +5,29 @@ public class EditDistanceStrings
 {
     final static int ERROR_INPUT = -1;
     
-    private int min(int a, int b) {
-        return (a<b)?a:b;
-    }
-    
-	    private int min(int a, int b, int c) {
-	        return min(min(a,b),c);
-	    }
-	    
-	    public int findDistance(String str1, String str2, int m, int n) {
-	        if (str1 == null || str2 == null)
-	            return ERROR_INPUT;
-	        
-	        if (m == 0)
-	            return n;
-	        
-	        if (n == 0)
-	            return m;
-	        
-	        if (str1.charAt(m-1) == str2.charAt(n-1))
-	            return findDistance(str1, str2, m-1, n-1);
-	         
-	        return min (
-	                     1 + findDistance(str1, str2, m-1, n),
-	                     1 + findDistance(str1, str2, m, n-1),  
-	                     1 + findDistance(str1, str2, m-1, n-1) 
-	                   );
-	    }
-	    
-	    public int findDistance(String str1, String str2){
-	        if (str1 == null || str2 == null)
+	private int findDistance_rec(String str1, String str2) {
+		return findDistance_rec(str1, str2, 0, 0);
+	}
+
+	// We can improve this recursion by using memoization with str1+str2 and count.
+	private int findDistance_rec(String str1, String str2, int idx1, int idx2) {
+		if(str1.length() == idx1)
+			return str2.length() - idx2;
+		if(str2.length() == idx2)
+			return str1.length() - idx1;
+		
+		if(str1.charAt(idx1) == str2.charAt(idx2))
+			return findDistance_rec(str1, str2, idx1+1, idx2+1);
+		else {
+			return 1 + Math.min(findDistance_rec(str1, str2, idx1, idx2+1), // Insert
+					Math.min(findDistance_rec(str1, str2, idx1+1, idx2+1), // Update
+							findDistance_rec(str1, str2, idx1+1, idx2) // Delete
+					));
+		}
+	}
+	
+	public int findDistance(String str1, String str2){
+	     if (str1 == null || str2 == null)
 	            return ERROR_INPUT;
 	        
 	        int[][] distanceTable = new int[str1.length()+1][str2.length()+1];
@@ -52,23 +45,20 @@ public class EditDistanceStrings
 	            else if (str1.charAt(m-1) == str2.charAt(n-1))
 	                distanceTable[m][n] = distanceTable[m-1][n-1]; 
 	            else
-	            	distanceTable[m][n] = min (
-	                    1 + distanceTable[m-1][n],
+	            	distanceTable[m][n] = Math.min(
+	                    1 + distanceTable[m-1][n], Math.min(
 	                    1 + distanceTable[m][n-1],
 	                    1 + distanceTable[m-1][n-1]
-	                );
+	                ));
 	            }
 	        }
 	        
 	        return distanceTable[numRows-1][numCols-1];
 	    }
 	    
-	    
-	    public static void main(String[] args) 
-	    {
+	    public static void main(String[] args) {
 	        EditDistanceStrings solution = new EditDistanceStrings();
-	
-	        System.out.print("minimum edit distance between \"intention\" and \"execution\" is: \n" );
-	        System.out.println(solution.findDistance("intention" , "execution" ));
+	        System.out.println("2D table: " + solution.findDistance("intention" , "execution" ));
+	        System.out.println("Recursion: " + solution.findDistance_rec("intention" , "execution" ));
 	    }
 }
